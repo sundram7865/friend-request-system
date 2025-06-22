881dd929ad00745f3999d0b980e516b70cb41d8e
sundram <ram322222@gmail.com>
2025-06-22 13:08:03 +0530

    created error handler

---
+++ backend/middleware/error.js
@@ -0,0 +1,16 @@
+const logger = require('../utils/logger');
+
+module.exports = (err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || 'error';
+
+  // Log the error
+  logger.error(`${err.statusCode} - ${err.message}`);
+
+  // Send response
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
+    ...(process.env.NODE_ENV === 'development' && { stack: err.stack })
+  });
+};
